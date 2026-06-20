b1b564dd1f72f5e3f54b80fca11b9260555b74d2
yinyanan-ctrl <20110700166@fudan.edu.cn>
2026-06-20 15:18:08 +0800

    Refactor PCA code and improve plotting
    Refactor PCA analysis and plotting code in NBE-PCA.R.

--- NBE-PCA.R
+++ NBE-PCA.R
@@ -1,35 +1,18 @@
-
-
 rm(list = ls())
 graphics.off()
-
 library(factoextra)
 library(ggplot2)
-
-# 读取数据
 unique_0_GP <- read.csv("pca-1.csv", row.names = 1)
 GPs <- t(unique_0_GP)
-
-# 数据预处理
 GPs[GPs == 0] <- 1e-5
 GPs <- log(GPs)
-
-# PCA
 GPs.pca <- prcomp(GPs)
-
-# 提取 PCA 坐标
 df <- as.data.frame(GPs.pca$x[, 1:2])
-
-# 分组信息
 df$Group <- factor(
   c(rep("Ctrl", times = 541), rep("PD", times = 2084)),
   levels = c("Ctrl", "PD")
 )
-
-# 解释方差
 var_exp <- round(GPs.pca$sdev^2 / sum(GPs.pca$sdev^2) * 100, 1)
-
-# 绘图
 p <- ggplot(df, aes(x = PC1, y = PC2, colour = Group, shape = Group)) +
   geom_point(
     aes(size = Group),
@@ -75,13 +58,10 @@ p <- ggplot(df, aes(x = PC1, y = PC2, colour = Group, shape = Group)) +
     legend.position = "right",
     legend.title = element_blank(),
     panel.grid.minor = element_blank(),
-    # ??? 添加黑色坐标轴
     axis.line = element_line(colour = "black", linewidth = 0.8),
     axis.ticks = element_line(colour = "black", linewidth = 0.8),
     axis.text = element_text(colour = "black", size = 12)
   )
-
-# 保存 PDF
 ggsave(
   filename = "PCA-1.pdf",
   plot = p,
